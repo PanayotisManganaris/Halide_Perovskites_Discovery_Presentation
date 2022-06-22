c996b73c6607648683823bd60b4aab8f12761590
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-06-21 23:26:41 -0400

    engraving sort of works.

--- pretty_pipeline.py
+++ pretty_pipeline.py
@@ -20,19 +20,17 @@ df = df.groupby('Formula', as_index=False).agg(
 dc = df.ft.comp()
 dc = dc.assign(label='label')
 
-numeric_features = dc
-.select_dtypes(np.number)
-.columns
-.to_list()
+numeric_features = dc.select_dtypes(np.number).columns.to_list()
 numeric_pipeline = make_pipeline(NumPreProcessor1(),
                                  NumPreProcessor2())
-categorical_features = mc
-.select_dtypes('object')
-.columns
-.to_list()
+categorical_features = mc.select_dtypes('object').columns.to_list()
 catagorical_pipeline = make_pipeline(CatPreProcessor1(),
                                      CatPreProcessor2())
 
+ss = ShuffleSplit(n_splits=1, train_size=0.8, random_state=None)
+train_idx, test_idx = next(ss.split(dc))
+dc_tr, dc_ts = dc.iloc[train_idx], dc.iloc[test_idx]
+df_tr, df_ts = df.iloc[train_idx], df.iloc[test_idx]
 
 preprocessor = ColumnTransformer(
     transformers=[
@@ -40,17 +38,8 @@ preprocessor = ColumnTransformer(
         ("cat", categorical_pipline, categorical_features),
     ]
 )
-
-ss = ShuffleSplit(n_splits=1, train_size=0.8,
-                  random_state=None)
-train_idx, test_idx = next(ss.split(dc))
-dc_tr, dc_ts = dc.iloc[train_idx], dc.iloc[test_idx]
-df_tr, df_ts = df.iloc[train_idx], df.iloc[test_idx]
-
 pipe = make_pipeline(preprocessor, Estimator())
-
 pipe.fit(dc_tr, df_tr.<target>)
-
 
 p, data = parityplot(pipe,
                      dc_ts, df_ts.<target>.to_frame(),
